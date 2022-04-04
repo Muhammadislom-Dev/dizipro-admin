fd253484fe6873cf6147be6ce0466e480ed6ae4b
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-04 22:28:38 +0500

    commit

--- src/Components/Aside/Aside.jsx
+++ src/Components/Aside/Aside.jsx
@@ -1,9 +1,39 @@
 import './Aside.scss'
 
+//Images
+import Chair from '../../assets/img/chair.png';
+import Table from '../../assets/img/table.png';
+import City from '../../assets/img/city.png';
+
 const Aside = () =>{
     return(
         <div className="aside">
-            
+            <p className="aside__back">
+               Back
+            </p>
+            <div className="aside__list">
+                <h2 className="aside__nam">3D armchair <br /> <span className='aside__id'>ID: 235462</span> </h2>
+                <button className="aside__btn">
+                    Submit project
+                </button>
+            </div>
+            <div className="aside__page">
+                <p className="aside__back">
+                    Project overview
+                </p>
+                <p className="aside__text">
+                Model contain 4 parts ready for print with keys and also a presupported version. This model on 100% scale is 16cm tall, but can be printed larger.
+                </p>
+                <p className="aside__back">
+                    Photos
+                </p>
+                <ul className="aside__lists">
+                    <li className="aside__item">
+
+                    </li>
+                </ul>
+            </div>
+
         </div>
     )
 }
--- src/Page/Weight/Weight.jsx
+++ src/Page/Weight/Weight.jsx
@@ -1,10 +1,12 @@
 import './Weight.scss'
 import PageContent from '../../Components/PageContent/PageContent'
+import Aside from '../../Components/Aside/Aside'
 
 const Weight = () =>{
     return(
         <div className="weight">
             <PageContent />
+            <Aside />
         </div>
     )
 }
