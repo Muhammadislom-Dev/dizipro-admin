c70e0462e54c1be3bf26764e50579cbad6e40b43
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-04 22:13:20 +0500

    commit

---
+++ src/Page/Weight/Weight.jsx
@@ -0,0 +1,13 @@
+import './Weight.scss'
+
+import HomeContents from '../../Components/HomeContents/HomeContents'
+
+const Weight = () =>{
+    return(
+        <div className="weight">
+            <HomeContents />
+        </div>
+    )
+}
+
+export default Weight
